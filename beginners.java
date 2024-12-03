a5ea0adb18c41a14d5af525c95fe713dbd26288a
Wannn Sion <wannnsion95@gmail.com>
2024-12-04 00:07:00 +0700

    Create beginners.java
    start with my biodata

---
+++ beginners.java
@@ -0,0 +1,18 @@
+
+public class Wann {
+
+    public static void main(String[] args) {
+
+        String name = "Name: Mhd. Ridwan";
+        String age = "Age: 19";
+        String Country = "Country: Indonesia";
+        String Hobby = "Hobby: Playing Video Games";
+        String Campus = "Campus: Politeknik Elektronika Negeri Surabaya";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(Country);
+        System.out.println(Hobby);
+        System.out.println(Campus);
+    }
+}
